032e721abbf32d5df248c6c3101cd338c9452b49
pshastri <praveen.shastri86@gmail.com>
2016-08-17 16:56:05 +0530

    my first code on git

---
+++ myStack.py
@@ -0,0 +1,15 @@
+import sys
+
+class myStack():
+	def __init__(self):
+		self.stack=[]
+	def stack_empty(self):
+		return self.stack==[]
+	def stack_push(self,item):
+		self.stack.append(item)
+	def stack_pop(self):
+		return self.stack.pop()
+	def stack_peek(self):
+		return self.stack[len(stack)-1]
+	def stack_size(self):
+		return len(self.stack)
---
+++ useStack.py
@@ -0,0 +1,6 @@
+from myStack import myStack
+
+if __name__=='__main__':
+	st=myStack()
+	print st.stack_empty()
+	
